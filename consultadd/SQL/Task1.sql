6afeafe7d83622e122578b40ab6b576419fe81fa
Shashank Dave <shashankdave58@gmail.com>
2022-09-28 21:56:14 -0400

    adding changes

---
+++ consultadd/SQL/Task1.sql
@@ -0,0 +1,8 @@
+INSERT INTO `customers` (`Cust_ ID`, `Cust_name`, `Cust_city`,`Cust_RoomNo`,`CheckIn_Time`) VALUES
+(1, 'shashank', 'Jersey City', '301', '2022-09-28 17:48:50'),
+(2, 'Ram', 'New York', '317', '2022-09-18 16:48:50'),
+(3, 'Sita', 'Ohio', '202', '2022-09-14 16:30:50'),
+(4, 'John', 'Texas', '116', '2022-09-19 15:42:50'),
+(5, 'Karan', 'California', '405', '2022-09-18 22:50:20');
+
+select * from customers
